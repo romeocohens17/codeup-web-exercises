52d558afa71afdf5f8049eb85a8522d35cb5361f
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-04-20 11:32:03 -0500

    Function Drills

--- js/function.drills.js
+++ js/function.drills.js
@@ -11,3 +11,19 @@ function isOdd(x) {
 }
 
 console.log(isOdd(10));
+
+//2.
+
+function isEven(x) {
+    if (isNaN(parseFloat(x))) {
+        return false;
+    }else {
+        return parseFloat(x) % 2 === 0;
+    }
+}
+
+console.log(isEven(4));
+
+//3.
+
+
