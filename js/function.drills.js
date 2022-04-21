580e4d239be5d49221f58f51c08e59dde8d51402
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-04-20 21:29:11 -0500

    function drill

--- js/function.drills.js
+++ js/function.drills.js
@@ -26,4 +26,39 @@ console.log(isEven(4));
 
 //3.
 
+function Identity(input) {
+    return input;
+}
+
+console.log(Identity("Hello"));
+
+//4.
+
+function isFive(input) {
+    return input === 5;
+}
+
+console.log(isFive(5));
+
+//5.
+
+function addFive(input) {
+    return parseFloat(input) + 5;
+}
+
+console.log(addFive(75));
+
+//6.
+
+function multipleOfFive(i) {
+    if (typeof(parseFloat(i) % 5 === 0)) {
+    }
+}
+
+console.log(multipleOfFive(75));
+
+//7.
+
+
+
 
