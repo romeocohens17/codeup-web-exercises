7af89950a690d86c97611696302b8b11e8df550e
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-04-25 09:17:58 -0500

    Finished the Planets Array

--- js/planets-array.js
+++ js/planets-array.js
@@ -35,10 +35,14 @@
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log(planets.indexOf("Earth"));
 
     console.log("Reversing the order of the planets array.");
+    planets.reverse();
     console.log(planets);
 
     console.log("Sorting the planets array.");
+    planets.sort();
     console.log(planets);
+
 })();
