f19c356ce38a75b804cae5baa2fb1f90d3abf50d
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-05-10 09:24:26 -0500

    finished the warmup

--- js/warmups.js
+++ js/warmups.js
@@ -180,3 +180,62 @@ function returnAllProductEssentialDetails(products) {
 })
 }
 
+//05/10
+
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "Spetember 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
+
+function mostColorful(hamsters) {
+    let mostColors = {fur : []};
+    for (let hamster of hamsters) {
+        if (hamster.fur.length > mostColors.fur.length) {
+            mostColors = hamster;
+        }
+    }
+    return mostColors;
+}
+
+function mostColorfullest(hamsters) {
+    let mostColorist = hamsters[0];
+    for (let x = 1; x < hamsters.length; x++) {
+        if (hamsters[x].fur.length > mostColorist.fur.length) {
+            mostColorist = hamsters[x];
+        }
+    }
+    return mostColorist;
+}
+
+console.log(mostColorfullest(hamsters));
+console.log(mostColorfullest(hamsters));
+
