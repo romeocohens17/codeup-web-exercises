a469d721325833877c06f546481f36c2522ec054
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-05-12 11:01:16 -0500

    self study

--- js/classwork/self_study.js
+++ js/classwork/self_study.js
@@ -1,8 +1,22 @@
 "use strict";
 
+
+//Arrays
 const friend1 = 'Michael';
 const friend2 = 'Jackie';
 const friend3 = 'Steven';
 
 const friends = ['Michael', 'Jackie', 'Steven'];
 console.log(friends);
+
+const years = new Array(1994, 1997, 2000);
+
+console.log(years);
+
+function forLoop(friends) {
+    for (let i = 0; i < friends.length; i++) {
+        console.log(friends[i])
+    }
+}
+
+console.log(forLoop(friends));
