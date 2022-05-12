9b4988a40a0b229a537ee88adf402b3c631b443f
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-05-12 13:39:50 -0500

    self study

--- js/classwork/self_study.js
+++ js/classwork/self_study.js
@@ -13,10 +13,45 @@ const years = new Array(1994, 1997, 2000);
 
 console.log(years);
 
-function forLoop(friends) {
-    for (let i = 0; i < friends.length; i++) {
-        console.log(friends[i])
+function forOf(array) {
+    for(let friend of friends) {
+        return friend
+    }
+}
+
+function forLoop(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i])
     }
 }
 
 console.log(forLoop(friends));
+console.log(forLoop(years));
+
+function forEachLoop(arr) {
+    let bucket = [];
+    arr.forEach(function(elem) {
+        if(typeof elem === "string") {
+            bucket.push(elem)
+        }
+    })
+    return bucket
+}
+
+console.log(forEachLoop(friends));
+console.log(forEachLoop(years));
+
+const bread = ['white', 'wheat']
+const fillings = ['ham', 'pb&j', 'steak']
+
+function makeSandwich(arr1, arr2) {
+    let bucket = [];
+    let s1 = {}
+        let s2 = {}
+    s1.bread = arr1[0];
+    s1.fillings = arr2[0];
+    s2.bread = arr1[1];
+    s2.fillings = arr2[2];
+    bucket.push(s1, s2)
+}
+console.log(makeSandwich(bread, fillings))
