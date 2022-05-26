2b9b50459cf05912068daa3efc720b6524da95ac
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-05-26 14:08:32 -0500

    saving work on open weather exercise

--- js/open-weather-js.js
+++ js/open-weather-js.js
@@ -1,10 +1,26 @@
 "use strict";
 
-const URL = "https://api.openweathermap.org/data/2.5/onecall"
+const URL = "https:api.openweathermap.org/data/2.5/onecall";
 
 $.get(URL, {
     APPID: OPEN_WEATHER_KEY,
     lat: 40.7485452,
     lon: -73.9879522,
     units: "imperial"
-}).done(function(results) {console.log(results);})
+}).done(function (results) {
+    console.log(results);
+})
+
+geocode("San Antonio, US", MAPBOX_API_KEY).then(function (results) {
+    return results;
+}).then(function (data){
+    console.log(data);
+    $.get(URL, {
+        APPID: OPEN_WEATHER_KEY,
+        lat:data[1],
+        lon: data[0],
+        units: "imperial"
+    }).done(function (results) {
+        console.log(results)
+    })
+})
