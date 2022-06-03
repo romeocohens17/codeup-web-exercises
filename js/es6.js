bd72f1ef1a965afa4787197b96cbd239a2769c07
Romeo Cohens <romeo.l.cohens@gmail.com>
2022-06-03 11:24:45 -0500

    saving work

--- js/es6.js
+++ js/es6.js
@@ -33,9 +33,9 @@ const users = [
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-var name = 'your_name_here';
-var email = '';
-var languages = [];
+const name = 'your_name_here';
+const email = '';
+const languages = [];
 
 // TODO: rewrite the object literal using object property shorthand
 users.push({
